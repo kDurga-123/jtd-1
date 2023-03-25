cc3cde04000cc7f10feafccc16876feaba78c719
kDurga-123 <119414747+kDurga-123@users.noreply.github.com>
2023-03-25 15:51:19 +0530

    Add files via upload

---
+++ prime1.java
@@ -0,0 +1,15 @@
+public class main{
+     public static void main(String[] args){
+         int num=10;
+         if(num%2!=0&&num%3!=0&&num%5!=0){
+             System.out.println("prime number");
+         }
+         else if (num==2||num==3||num==5){
+             System.out.println("prime number");
+         }
+         else{
+              System.out.println("not a prime number");
+         }
+            
+     }
+ }
